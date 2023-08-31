be3d0865c3d9b45e302e18d421fed1e159fc350f
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-09-01 01:06:07 +0200

    commit 3

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -5,21 +5,6 @@ import {toast} from "react-toastify";
 
 axios.defaults.baseURL = 'https://64f0fad28a8b66ecf77a4d5d.mockapi.io';
 
-const handleError = (e, api) => {
-    toast.error(e.message, {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-    });
-    return api.rejectWithValue(e.message);
-}
-
-
 export const fetchContacts = createAsyncThunk(
     "contacts/fetchAll",
     async (_, thunkAPI) => {
